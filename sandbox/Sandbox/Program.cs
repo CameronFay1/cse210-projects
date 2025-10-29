1b0a5f7eca0d7b5873219c6e8225f2a9a577f96d
cameron <fayminecraft6@hotmail.com>
2025-10-29 11:32:15 -0600

    exercise1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project. test");
+
+        Console.Write("Input: ");
+        string input = Console.ReadLine();
+        Console.WriteLine($"your input is ({input})");
     }
 }
