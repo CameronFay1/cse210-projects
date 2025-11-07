6546914bf37847db2f75faae5cca2a5046008245
cameron <fayminecraft6@hotmail.com>
2025-11-07 10:09:55 -0700

    journal 2

--- week02/Journal/randomPrompt.cs
+++ week02/Journal/randomPrompt.cs
@@ -3,7 +3,12 @@ using System.Runtime.Serialization;
 
 public class RandomPrompt
 {
-    public List<string> _prompt = new List<string>{"one", "two", "three"};
+    public List<string> _prompt = new List<string>
+    {"What was one win today?", "Whats one thing I learned today?",
+    "What are three things Im grateful for right now?", "How can I be 1% better tomorrow?",
+    "What can I forgive myself for?",
+    "Whats something that always makes me smile?",
+    "Whats one small moment I want to remember forever?"} ;
     public string GetRandomPrompt()
     {
         Random randomGenerator = new Random();
