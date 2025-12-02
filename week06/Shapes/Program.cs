b93d9b8ca7abfb4246511ffa999d67e279f61b3d
cameron <fayminecraft6@hotmail.com>
2025-12-02 11:34:01 -0700

    Shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shapes> list = new List<Shapes>{};
+        Square S1 = new Square("Green",4);
+        list.Add(S1);
+        Square S2 = new Square("red",7);
+        list.Add(S2);
+        Rectangle R1 = new Rectangle("black", 2,5);
+        list.Add(R1);
+        Rectangle R2 = new Rectangle("yellow", 6,9);
+        list.Add(R2);
+        Circle C1 = new Circle("gray",10);
+        list.Add(C1);
+        Circle C2 = new Circle("blue",3);
+        list.Add(C2);
+        foreach (Shapes i in list)
+        {
+            Console.WriteLine($"color: {i.GetColor()} Area: {i.GetArea()}");
+        }
+
     }
 }
